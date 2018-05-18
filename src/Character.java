00b403af1afebd7d34df2eda62051477f7225361
MattLips <mabzlips@gmail.com>
2018-05-18 17:15:48 -0400

    Roughly started character class.

---
+++ src/Character.java
@@ -0,0 +1,20 @@
+
+public class Character {
+	
+	private int health;
+	private int armor;
+	private int attack;
+	private int defense;
+	
+	
+	public Character() {
+		
+		health = 100;
+		armor = 0;
+		attack = 10;
+		defense = 10;
+		
+	}
+	
+	
+}
