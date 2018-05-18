998c7aa06427e8041dd47e33f8651f99d4dc2a22
MattLips <mabzlips@gmail.com>
2018-05-18 16:19:59 -0400

    Commented in hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World"); // Helloooooooooooooooooooooooooooooooooo
+        System.out.println("Hello, World"); // Hello
     }
 
 }
