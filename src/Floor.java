8fab61f27185121f7f7ed6ffc0447a4dafb5dc24
MattLips <mabzlips@gmail.com>
2018-05-19 19:06:58 -0400

    Made outline of floor class

--- src/Floor.java
+++ src/Floor.java
@@ -1,3 +1,24 @@
 
 public class Floor {
+
+	private Tile[][] floorTiles;
+
+	public Floor(Tile[][] arr) {
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 16; j++) {
+				floorTiles[i][j] = arr[i][j];
+			}
+		}
+
+	}
+	
+	public Tile getTile(int x, int y) {
+		return floorTiles[x][y];
+	}
+	
+	public void setTile(Tile tile, int x, int y) {
+		floorTiles[x][y] = tile;
+	}
+
 }
