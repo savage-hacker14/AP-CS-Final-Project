83c0c320fcd66f5851dc79bb0feea7791b32a5f3
MattLips <mabzlips@gmail.com>
2018-05-21 21:13:31 -0400

    Added getters and setters for the Character class

--- src/Character.java
+++ src/Character.java
@@ -6,6 +6,10 @@ public class Character extends Tile {
 	private int armor;
 	private int attack;
 	private int defense;
+	private int maxHealth;
+	private int maxArmor;
+	private int maxAttack;
+	private int maxDefense;
 	
 	
 	public Character(BufferedImage img) {
@@ -14,7 +18,102 @@ public class Character extends Tile {
 		armor = 0;
 		attack = 10;
 		defense = 10;
-		
+		maxHealth = 100;
+		maxArmor = 0;
+		maxAttack = 20;
+		maxDefense = 20;
+	}
+
+
+	public int getHealth() {
+		return health;
+	}
+
+	/* Increases health by health, but doesn't exceed maxHealth */
+	public void increaseHealth(int health) {
+		this.health += health;
+		if (this.health > maxHealth) {
+			this.health = maxHealth;
+		}
+	}
+
+
+	public int getArmor() {
+		return armor;
+	}
+
+	/* Decreases armor by armor, but doesn't go less than 0 */
+	public void damageArmor(int armor) {
+		this.armor -= armor;
+		if (this.armor < 0) {
+			this.armor = 0;
+		}
+	}
+
+
+	public int getAttack() {
+		return attack;
+	}
+
+	/* Increases attack by attack, but doesn't exceed maxAttack */
+	public void increaseAttack(int attack) {
+		this.attack += attack;
+		if (this.attack > maxAttack) {
+			this.attack = attack;
+		}
+	}
+
+
+	public int getDefense() {
+		return defense;
+	}
+
+	/* Increases defense by defense, but doesn't exceed maxDefense */
+	public void increaseDefense(int defense) {
+		this.defense += defense;
+		if (this.defense > maxDefense) {
+			this.defense = maxDefense;
+		}
+	}
+
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+
+	public void increaseMaxHealth(int maxHealth) {
+		this.maxHealth += maxHealth;
+	}
+
+
+	public int getMaxArmor() {
+		return maxArmor;
+	}
+
+
+	public void increaseMaxArmor(int maxArmor) {
+		this.maxArmor += maxArmor;
+	}
+
+
+	public int getMaxAttack() {
+		return maxAttack;
+	}
+
+
+	public void increaseMaxAttack(int maxAttack) {
+		this.maxAttack += maxAttack;
+	}
+
+
+	public int getMaxDefense() {
+		return maxDefense;
+	}
+
+
+	public void increaseMaxDefense(int maxDefense) {
+		this.maxDefense += maxDefense;
 	}
 	
 	
